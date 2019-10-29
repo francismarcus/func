58d858cec9fb9c288853a191fde04b1e8d37aafa
Marcus Francis <marcusmagnusson@outlook.com>
2019-10-29 03:21:59 +0100

    Update compose/index.ts
    Prettier

--- src/compose/index.ts
+++ src/compose/index.ts
@@ -1,3 +1,4 @@
-const compose = <F extends any[], G, R>(...funcs: F) => (x: G): R => funcs.reduceRight((g, f) => f(g), x);
+const compose = <F extends any[], G, R>(...funcs: F) => (x: G): R =>
+  funcs.reduceRight((g, f) => f(g), x);
 
 export default compose;
